5458883846ee558f45890610a0f8662110d055c7
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2018-07-18 16:44:29 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+ let escolha = function() {
+     let inputNome = document.getElementById('inputNome');
+     let inputEmpre = document.getElementById('inputEmpre');
+     let inputnotaF = document.getElementById('inputnotaF');
+     let inputData = document.getElementById('inputData');
+
+
+     document.getElementById("text").innerHTML = "Você selecionou:" + inputNome;
+ }
