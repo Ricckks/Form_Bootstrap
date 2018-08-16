347f54fb526c0d90e0f064e2f7ae73d453158447
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2018-08-16 15:40:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,26 @@
 
      document.getElementById("text").innerHTML = "Você selecionou:" + inputNome;
  }
+ 
+let validation = function() {
+     let inputNome = document.getElementById('inputNome');
+     let inputEmpre = document.getElementById('inputEmpre');
+     let inputnotaF = document.getElementById('inputnotaF');
+     let inputData = document.getElementById('inputData');
+
+
+     let formulario = [formName, formEmail, formConfirmaEmail, formSenha, formConfirmaSenha];
+
+     if (formulario[i]).value === '') {
+
+     alert("Preencha os Campos");
+
+ }
+
+ else {
+
+     alert("Dados Enviados");
+
+ }
+
+ }
