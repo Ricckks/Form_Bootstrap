0ed3bbf81647c6fa99207638a638ab720543920d
Ricardo Andrade <38958597+Ricckks@users.noreply.github.com>
2018-08-22 20:03:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
- let escolha = function() {
+let escolha = function() {
      let inputNome = document.getElementById('inputNome');
-     let inputEmpre = document.getElementById('inputEmpre');
-     let inputnotaF = document.getElementById('inputnotaF');
-     let inputData = document.getElementById('inputData');
+     let inputEmpresa = document.getElementById('inputEmpresa');
+     let inputnotaFis = document.getElementById('inputnotaFis');
+     let inputDate = document.getElementById('inputDate');
 
 
      document.getElementById("text").innerHTML = "Você selecionou:" + inputNome;
+     document.getElementById("legenda").innerHTML = "Você selecionou:" + inputEmpresa;
+     document.getElementById("texts").innerHTML = "Você selecionou:" + inputnotaF;
+     document.getElementById("txt").innerHTML = "Você selecionou:" + inputData;
  }
- 
-let validation = function() {
+
+ let validation = function() {
      let inputNome = document.getElementById('inputNome');
-     let inputEmpre = document.getElementById('inputEmpre');
+     let inputEmpresa = document.getElementById('inputEmpresa');
      let inputnotaF = document.getElementById('inputnotaF');
      let inputData = document.getElementById('inputData');
 
 
-     let formulario = [formName, formEmail, formConfirmaEmail, formSenha, formConfirmaSenha];
+     let formulario = [inputNome, inputEmpresa, inputnotaF, inputData];
 
      if (formulario[i]).value === '') {
 
